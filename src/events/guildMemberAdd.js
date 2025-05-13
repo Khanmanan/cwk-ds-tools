3eb2778a411dacd3230fb9a29e7dcab20d75b949
Cwkhan <65243374+Khanmanan@users.noreply.github.com>
2025-05-14 01:58:48 +0530

    Create guildMemberAdd.js

---
+++ src/events/guildMemberAdd.js
@@ -0,0 +1,9 @@
+module.exports = {
+    name: 'guildMemberAdd',
+    once: false,
+    
+    async execute(member, client) {
+        // Auto-modlog handles this in automodlog.js
+        // Additional custom logic can be added here
+    }
+};
