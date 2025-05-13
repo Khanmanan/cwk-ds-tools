1ea4109c32bd2e5bb21bfe54def605e05e8e1594
Cwkhan <65243374+Khanmanan@users.noreply.github.com>
2025-05-14 01:59:14 +0530

    Create interactionCreate.js

---
+++ src/events/interactionCreate.js
@@ -0,0 +1,21 @@
+module.exports = {
+    name: 'interactionCreate',
+    once: false,
+
+    async execute(interaction, client) {
+        if (!interaction.isChatInputCommand()) return;
+
+        const command = client.commands.get(interaction.commandName);
+        if (!command) return;
+
+        try {
+            await command.run(interaction, client);
+        } catch (error) {
+            console.error(error);
+            await interaction.reply({
+                content: 'There was an error while executing this command!',
+                ephemeral: true
+            });
+        }
+    }
+};
